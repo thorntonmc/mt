340b43f5734fa9633e8e2ba0388ca88906acbd38
thorntonmc <mt@mthornton.dev>
2022-02-04 19:32:27 -0500

    stay generic

--- internal/mtee.go
+++ internal/mtee.go
@@ -96,7 +96,7 @@ func (m *mtee) tee() error {
 	text := fmt.Sprintf("%s\n", m.scanner.Text())
 
 	for _, v := range m.out {
-		go func(v *os.File) {
+		go func(v io.Writer) {
 			_, err := v.Write([]byte(text))
 			results <- newTeeResult(err)
 		}(v)
